a7b7f65ac571f19d0c404436b343b0677bfbbcca
Parmjeet <parmjeetmishra2@gmail.com>
2023-12-23 00:27:18 +0530

    Update to label

--- src/components/ui/Form.jsx
+++ src/components/ui/Form.jsx
@@ -22,14 +22,18 @@ const Form = () => {
 
       <form onSubmit={handleSubmit} className="w-full max-w-md ">
     
-        <Label htmlFor="name" title="Your Name" />
+      <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        Your Name
+      </label>
         <div className="relative mb-2">
           <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
             <NameIcon className="w-5" />
           </div>
           <Input type="text" id="name" placeholder="Enter your name" />
         </div>
-        <Label htmlFor="email" title="Your Email" />
+        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        Your Email
+      </label>
         <div className="relative mb-4">
           <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
             <EmailIcon className="w-5" />
